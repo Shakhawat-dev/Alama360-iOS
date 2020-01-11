eba59d9c163b1d9855eef88508d963631579085c
Shakhawat <shakhawat.aisoftz@gmail.com>
2020-01-11 18:01:53 +0300

    top menu bar 11/01/2020

--- Alama360/Controllers/FAQViewController.swift
+++ Alama360/Controllers/FAQViewController.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
+
 class FAQViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupManuBar()
     }
     
+    let menubar: MenuBar = {
+        let mb = MenuBar()
+        return mb
+    }()
+    
+    private func setupManuBar() {
+//        view = UIView(frame: CGRect(x: 0,y: (navigationController?.navigationBar.frame.height)!,width: 0,height: 50))
+//        let mbView = UIView(frame: CGRect(x: 0, y: (navigationController?.navigationBar.frame.height)!, width: UIScreen.main.bounds.width, height: 200))
+        view.addSubview(menubar)
+        view.addConstraintsWithFormat("H:|[v0]|", views: menubar)
+        view.addConstraintsWithFormat("V:|[v0(130)]|", views: menubar)
+            
+    }
 
 }
+
+extension UIView {
+    func addConstraintsWithFormat(_ format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+        }
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
+
---
+++ Alama360/Controllers/MenuBar.swift
@@ -0,0 +1,44 @@
+//
+//  MenuBar.swift
+//  Alama360
+//
+//  Created by Alama360 on 16/05/1441 AH.
+//  Copyright © 1441 Alama360. All rights reserved.
+//
+
+import UIKit
+
+class MenuBar: UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = #colorLiteral(red: 0.02439633612, green: 0.6269035533, blue: 0.4417837247, alpha: 1)
+        cv.delegate = self
+        cv.dataSource = self
+
+        return cv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(collectionView)
+        addConstraintsWithFormat("H:|[v0]|", views: collectionView)
+        addConstraintsWithFormat("V:|[v0]|", views: collectionView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+}
